1f83b1c2d04b535d828624756ebebebc5222e33a
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-01-29 13:47:48 +0200

    fetching the data from fake api using json-server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
-
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+function reducer(state, action){
+    switch (action.type) {
+        case "dataReceived":
+          return {
+            ...state,
+            questions: action.payload,
+            status: "ready",
+          }
+        case "dataFailed":
+          return {
+            ...state,
+            status: "error",
+          }  
+        default:
+          throw new Error("Unknown action");  
+    }
+}
 
 export default function App() {
 
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  async function fetchingData() {
+    try {
+      const res = await fetch("http://localhost:8000/questions");
+      const data = await res.json();
+      dispatch({type: 'dataReceived', payload: data});
+    } catch(err) {
+      dispatch({type: 'dataFailed'});
+    }
+  }
+
+  useEffect(function () {
+    fetchingData();
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header/>
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
